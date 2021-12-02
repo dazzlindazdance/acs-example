e2201e1d9b0145bc4143f3a415dc7669e4a203f7
Darren Dance <ddance@splunk.com>
2021-12-02 16:07:44 +0000

    improved parameter parsing and usecases

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # acs-example
 Examples on how ACS can be used to manage configuration and deploy apps to Splunk Cloud
+# Basic Python App Install Example
+Python Script: app_install.py
+This python script takes a bunch of args, run with -h to check them all...
+This python script submits the app to appinspect, and if the app passes appinspect it will attempt to install the app on the specified cloud stack.
+If the app fails appinspect then it will write the failure report out to a json file in the same directory as the app, subject to write permissions.
+
+Example Usage:
+python3 app_install.py --stack flash-monkey-02 --token stack_jwt.txt --username someuser@splunk.com --classic ../subjective-sample-app_102.tgz
--- python_example/app_install.py
+++ python_example/app_install.py
@@ -1,42 +1,74 @@
 # import requests module
+from os import error
 import requests
 from requests.auth import HTTPBasicAuth
 import getpass
 import json
 import time
 import sys
+import argparse
 
-# current expected input until i add useful helper and flags:
-#
-# python3 app_install.py <stack> <app_installer> <file_with_stack_jwt>
-#
-# python3 app_install.py flash-monkey-02 ../subjective-sample-app_100.tgz stack_jwt.txt
-stack=sys.argv[1]
-appToInstall=sys.argv[2]
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-s","--stack",type=str,help="Please Specify the stack your jwt relates to that you wish to install the app on.")
+parser.add_argument("-t","--token",type=str,help="Please Specify the file containing the jwt that will allow you to install the app.")
+parser.add_argument("-u","--username",type=str,help="Please Specify the Splunk.com username for authentication to appinspect, you will be prompted for the password.")
+parser.add_argument("--staging", help="Flag to pass if this is a Splunk Cloud Staging Stack [Internal Use Only]", action="store_true")
+parser.add_argument("--classic",help="Pass this flag if you are installing to a Splunk Cloud Classic Stack",action="store_true")
+parser.add_argument("app_targz",type=str,help="The app you wish to vet and install")
+args = parser.parse_args()
+
+if args.stack:
+    stack=args.stack
+else:
+    print("Stack Not Specified.")
+
+if args.token:
+    jwt_file=args.token
+    # read stack Java Web Token from file, file defined as arg[3] presently until i can add some better flags to my script
+    with open(jwt_file,'r') as file:
+        stack_jwt = file.read().rstrip()
+else:
+    print("Token file not specified.")
+
+if args.staging:
+    acs_uri = "staging.admin.splunk.com"
+    print("We will attempt to install the app to an internal staging stack of Splunk Cloud")
+else:
+    acs_uri = "admin.splunk.com"
+
+if args.classic:
+    appinspect_tags="private_app"
+else:
+    appinspect_tags="cloud,self-service"
 
 
-# read stack Java Web Token from file, file defined as arg[3] presently until i can add some better flags to my script
-jwt_file=sys.argv[3]
-with open(jwt_file,'r') as file:
-    stack_jwt = file.read().rstrip()
+if args.app_targz:
+    appToInstall = args.app_targz
+else:
+    print("App not specified.")
 
-user = input("Enter splunk.com username:")
-print(user)
-password = getpass.getpass(prompt='Enter password:',stream=None)
-print(password)
+if args.username:
+    user = args.username
+    password = getpass.getpass(prompt='Enter splunk.com password:',stream=None)
+else:
+    print("username not specified.")
 
 # Making a authentication request and get token for appinspect
 response = requests.get('https://api.splunk.com/2.0/rest/login/splunk', auth = HTTPBasicAuth(user, password))
 response_json=json.loads(response.text)
-token=response_json["data"]["token"]
+if response_json["data"]["token"]:
+    token=response_json["data"]["token"]
+else:
+    print("Probably an authentication error. Did you enter the right password?")
 
 
 # submitting the app tarball to appinspect
 appinspect_uri="https://appinspect.splunk.com/v1/app/validate"
 files={'app_package': open(appToInstall,'rb')}
 header={"Authorization": "Bearer "+token}
-appinspect_response = requests.post(appinspect_uri,headers=header,files=files,data={"included_tags":"private_app"})
-# The above tags are for Classic stack , please replace with "cloud,self-service" when using Victoria
+appinspect_response = requests.post(appinspect_uri,headers=header,files=files,data={"included_tags":appinspect_tags})
 appinspect_response_json=json.loads(appinspect_response.text)
 
 # grabbing status uri
@@ -52,7 +84,7 @@ while appinspect_status_json["status"]=="PROCESSING":
     time.sleep(30)
     appinspect_status_response = requests.get("https://appinspect.splunk.com"+appinspect_status,headers=header)
     appinspect_status_json = json.loads(appinspect_status_response.text)
-print(appinspect_status_response.text)
+#print(appinspect_status_response.text)
 
 # not it's been inspected we can work out what to do next...
 if appinspect_status_json["status"]=="SUCCESS" and appinspect_status_json["info"]["failure"]>0:
@@ -64,11 +96,11 @@ if appinspect_status_json["status"]=="SUCCESS" and appinspect_status_json["info"
         print("Failure Report written to: "+appToInstall+"_failure_report.json")
 elif appinspect_status_json["status"]=="SUCCESS" and appinspect_status_json["info"]["failure"]==0:
     print("Starting Install...")
-    print("Token: "+token)
-    print("stack_jwt: "+stack_jwt)
+    #print("Token: "+token)
+    #print("stack_jwt: "+stack_jwt)
     print("")
     # I am using an internal staging stack, please change URL to admin.splunk.com here when using production stack
-    install_uri="https://staging.admin.splunk.com/"+stack+"/adminconfig/v2/apps"
+    install_uri="https://"+acs_uri+"/"+stack+"/adminconfig/v2/apps"
     files={'package': open(appToInstall,'rb')}
     payload={'token':token}
     header={"Authorization": "Bearer "+stack_jwt,"ACS-Legal-Ack":"Y"}
